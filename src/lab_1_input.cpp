a923b10b90cecf94c1d8b34c36dabc415ab35988
danilden1 <danilden1@yandex.ru>
2020-10-12 20:18:09 +0300

    transmit data between pub and sub

--- src/lab_1_input.cpp
+++ src/lab_1_input.cpp
@@ -9,21 +9,22 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "str_publisher");
     ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<std_msgs::String>("/str", 10);
+    //ros::Publisher pub = n.advertise<std_msgs::String>("/str", 10);
+    ros::Publisher pub = n.advertise<lab_1::str3>("/str3", 10);
     ros::Rate loop_rate(1);
     //lab_1::str3;
-   // lab_1::str3 str;
-   // str.first_number = "10";
-   // str.second_number = "20";
-    //str.sign = "+";
+    lab_1::str3 str;
+    str.first_number = "10";
+    str.second_number = "20";
+    str.sign = "+";
     std_msgs::String st;
     st.data = "Hi!";
     while(1)
     {
-        //cout << str.first_number << endl;
-        //cout << str.sign << endl;
-        //cout << str.second_number << endl;
-        pub.publish(st);
+        cout << str.first_number << endl;
+        cout << str.sign << endl;
+        cout << str.second_number << endl;
+        pub.publish(str);
         loop_rate.sleep();
     }
     return 0;
--- src/lab_1_result.cpp
+++ src/lab_1_result.cpp
@@ -7,21 +7,25 @@ using namespace std;
 
 ros::Publisher pub;
 
-void recieve(const std_msgs::String& str)
+void resultCallback(const lab_1::str3& str)
 {
-    cout << str.data << endl;
-    pub.publish(str);
+    cout << str.first_number << endl;
+    cout << str.sign << endl;
+    cout << str.second_number << endl;
+    //pub.publish(str);
     return;
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "string_subscriber");
+    ros::init(argc, argv, "str_subscriber");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("/str", 10, recieve);
-    ros::init(argc, argv, "string_publisher");
-    ros::NodeHandle k;
-    pub = k.advertise<std_msgs::String>("/value", 10);
+    ros::Subscriber sub = n.subscribe("/str3", 10, resultCallback);
+    //ros::init(argc, argv, "result_publisher");
+   // ros::NodeHandle k;
+    //pub = k.advertise<lab_1::str3>("/value", 10);
+
+
 
     ros::spin();
 
