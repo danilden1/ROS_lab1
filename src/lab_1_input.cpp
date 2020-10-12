1db849e3c8128e626f3a81680948ec97443b058a
danilden1 <danilden1@yandex.ru>
2020-10-12 22:49:44 +0300

    translte random number and print result

--- src/lab_1_input.cpp
+++ src/lab_1_input.cpp
@@ -14,16 +14,50 @@ int main(int argc, char **argv)
     ros::Rate loop_rate(1);
     //lab_1::str3;
     lab_1::str3 str;
+
     str.first_number = "10";
     str.second_number = "20";
     str.sign = "+";
     std_msgs::String st;
     st.data = "Hi!";
+
+    int firs_value = 0;
+    int last_value = 10000;
     while(1)
     {
-        cout << str.first_number << endl;
-        cout << str.sign << endl;
-        cout << str.second_number << endl;
+        double random_number_first = (firs_value + rand() % last_value) / 100;
+        double random_number_second = (firs_value + rand() % last_value) / 100;
+
+        str.first_number = std::to_string(random_number_first);
+        str.second_number = std::to_string(random_number_second);
+
+        int random_number_sign = 1 + rand() % 4;
+        switch (random_number_sign)
+        {
+            case 1:
+            {
+                str.sign = "+";
+                break;
+            }
+            case 2:
+            {
+                str.sign = "-";
+                break;
+            }
+            case 3:
+            {
+                str.sign = "*";
+                break;
+            }
+            case 4:
+            {
+                str.sign = "/";
+                break;
+            }
+        }
+        //cout << str.first_number << endl;
+        //cout << str.sign << endl;
+        //cout << str.second_number << endl;
         pub.publish(str);
         loop_rate.sleep();
     }
--- src/lab_1_result.cpp
+++ src/lab_1_result.cpp
@@ -4,14 +4,50 @@
 #include "lab_1/str3.h"
 using namespace std;
 
+struct st
+{
+    double fn;
+    double sn;
+    char sign;
+    double res;
+};
 
+st answer;
 ros::Publisher pub;
 
 void resultCallback(const lab_1::str3& str)
 {
-    cout << str.first_number << endl;
-    cout << str.sign << endl;
-    cout << str.second_number << endl;
+    cout << str.first_number << " ";
+    cout << str.sign << " ";
+    cout << str.second_number << " = ";
+    answer.fn = std::stod(str.first_number);
+    answer.sn = std::stod(str.second_number);
+    answer.sign = str.sign[0];
+    answer.res = 0.0;
+    switch (answer.sign)
+    {
+        case 0x2B:
+        {
+            answer.res = answer.fn + answer.sn;
+            break;
+        }
+        case 0x2D:
+        {
+            answer.res = answer.fn - answer.sn;
+            break;
+        }
+        case 0x2A:
+        {
+            answer.res = answer.fn * answer.sn;
+            break;
+        }
+        case 0x2F:
+        {
+            answer.res = answer.fn / answer.sn;
+            break;
+        }
+    }
+    cout << answer.res << endl;
     //pub.publish(str);
     return;
 }
